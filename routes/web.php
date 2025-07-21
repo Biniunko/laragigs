068f484f637152623466711dfe85420d96aeb7f3
Biniunko <beninasefa@gmail.com>
2025-07-21 20:57:50 +0300

    intital commit

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/hello', function(){
+    return response("<h1>hello</h1>");
+});
+
+Route::get('/posts/{id}',function($id){
+    //dd($id); this is die and dump method for debugging 
+    //ddd(id); this is dump , die and debug method for debugging
+    return response('posts'.$id);
+})->where('id','[0-9]+');
